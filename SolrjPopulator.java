4624e68a2df89fb68cac5e9f9ce271c60676e3a0
Sreepada Rao Singeetham <sreepada.singeetham@gmail.com>
2015-03-28 13:49:00 -0700

    add pageRank part 1

--- SolrjPopulator.java
+++ SolrjPopulator.java
@@ -1,7 +1,31 @@
+import java.io.File; 
+import java.io.IOException;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+
+import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.SequenceFile;
+
+import org.apache.nutch.util.NutchConfiguration;
+import org.apache.nutch.crawl.Inlinks;
+import org.apache.nutch.crawl.Inlink;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.*;
+
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse; 
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.common.SolrDocumentList;
@@ -12,46 +36,93 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.io.File;
-import java.io.FileFilter;
+import java.io.*;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
+class pageRankUrl {
+    private String inLink;
+    private String baseUrl;
+    private int weight = 1;
+    public pageRankUrl (String inLink, String baseUrl) {
+        this.inLink = inLink;
+        this.baseUrl = baseUrl;
+    }
+    public String getInLink() {
+        return inLink;
+    }
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
 public class SolrjPopulator {
     public static void main(String[] args) throws IOException, SolrServerException {
-        //HttpSolrServer server = new HttpSolrServer("http://localhost:8983/solr");
-        //for(int i=0;i<1000;++i) {
-        //  SolrInputDocument doc = new SolrInputDocument();
-        //  doc.addField("cat", "book");
-        //  doc.addField("id", "book-" + i);
-        //  doc.addField("name", "The Legend of the Hobbit part " + i);
-        //  server.add(doc);
-        //  if(i%100==0) server.commit();  // periodically flush
-        //}
-        HttpSolrServer server = new HttpSolrServer("http://localhost:8983/solr");
+        Map<String, String> urlDidMap = new HashMap<String, String>();
+        ArrayList<pageRankUrl> graph = new ArrayList<pageRankUrl>();
+        try {
+            Configuration confForReader = NutchConfiguration.create();
+            FileSystem fs = FileSystem.get(confForReader);
+            Path segmentFile = new Path("/home/sreepada/Documents/CSCI_572/crawl_backup/crawl_old/linkdb/current/part-00000/data");
+            SequenceFile.Reader reader = new SequenceFile.Reader(fs, segmentFile, confForReader);
+            Text segmentKey = new Text();
+            Inlinks segmentInLink = new Inlinks();
+            // Loop through sequence files to get Content metadata of already
+            while (reader.next(segmentKey, segmentInLink)) {
+                Iterator<Inlink> currLink = segmentInLink.iterator();
+                urlDidMap.put(segmentKey.toString(), "dummyDid");
+                while(currLink.hasNext()) {
+                    String somehting = (currLink.next().toString().split(" ")[1]).trim();
+                    graph.add(new pageRankUrl(somehting, segmentKey.toString()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // for (pageRankUrl temp : graph) {
+        //     System.out.println(temp.getInLink() + " " + temp.getBaseUrl());
+        // }
+        // System.exit(0);
 
-        File dir = new File("/home/sreepada/Documents/CSCI_572/LoremIpsumSearch");
+        String urlString = "http://localhost:8983/solr"; 
+        SolrServer solr = new HttpSolrServer(urlString);
+
+        File dir = new File("/home/sreepada/Documents/CSCI_572/crawl_backup/dumpDir");
         FileFilter fileFilter = new WildcardFileFilter("*.html");
         File[] files = dir.listFiles(fileFilter);
+        String currDocID;
         for (int i = 0; i < files.length; i++)
         {
-            System.out.println(files[i]);
-            SolrInputDocument solr_doc = new SolrInputDocument();
+            currDocID = "html-" + i;
+            for(String key : urlDidMap.keySet()) {
+                if (key.substring(key.lastIndexOf("/") + 1).equals(files[i].toString().substring(files[i].toString().lastIndexOf("/") + 1))) {
+                    urlDidMap.put(key, currDocID); 
+                }
+            }
+            ContentStreamUpdateRequest up = new ContentStreamUpdateRequest("/update/extract");
 
-            //File input = new File("/home/madan/usc_notes/spring_2015/ir/nutch/runtime/local/alldata/Wei2011.html");
-            //File input = new File(files[i]);
+            up.addFile(new File(files[i].toString()), "text/plain");
+
             Document doc = Jsoup.parse(files[i], "UTF-8", "http://example.com/");
-            System.out.println(doc.title());
-            Elements elements = doc.body().getElementsByClass("descriptive_row_table");
-            solr_doc.addField("id" , "html-" + i);
-            solr_doc.addField("title", doc.title());
-            System.out.println("Title=" + doc.title());
+            Elements elements;
+            try {
+                elements = doc.body().getElementsByClass("descriptive_row_table");
+            }
+            catch(NullPointerException e) {
+                System.out.println("ullaasdfasdf");
+                continue;
+            }
+            up.setParam("literal.id", currDocID);
+            up.setParam("id", currDocID);
+            up.setParam("literal.title", doc.title());
             Double bboxValues[] = new Double[4];
+
             for(Element element: elements)
             {
                 String text = element.text();
                 String[] key_val_pair = text.split(":", 2);
-                System.out.println(key_val_pair.length);
+                //               System.out.println(key_val_pair.length);
                 String key = key_val_pair[0].toLowerCase().replace(" ", "_");
                 key = key + "_s";
                 String value = key_val_pair[1].replace(" > ", ",");
@@ -60,7 +131,7 @@ public class SolrjPopulator {
                     value = value.replaceAll("^[ ()]+", "").replaceAll("[ ()]+$", "");
                     value = value.split(" ")[0];
                 }
-                System.out.println("key=" + key + "\tvalue=" + value);
+                //                System.out.println("key=" + key + "\tvalue=" + value);
                 if (key.contains("northernmost_l")) 
                     bboxValues[2] = Double.parseDouble(value);
                 else if (key.contains("southernmost_l"))
@@ -70,18 +141,32 @@ public class SolrjPopulator {
                 else if (key.contains("easternmost_l"))
                     bboxValues[1] = Double.parseDouble(value);
                 else
-                    solr_doc.addField(key, value);
+                    up.setParam("literal." + key, value);
             }
-            solr_doc.addField("bbox", "ENVELOPE(" + bboxValues[0] + ", "
-                    + bboxValues[1] + ", " + bboxValues[2] + ", " + bboxValues[3] + ")");
-            server.add(solr_doc);
-            server.commit();
-
-            QueryResponse rsp = server.query(new SolrQuery("id:html-0"));
-            SolrDocumentList results = rsp.getResults();
-            System.out.println("--------------------------------------------------------------------------------------------------------------");
-            for (int k=0; k < results.size(); k++)
-                System.out.println(results.get(k));
+            if (bboxValues[0] != null) {
+                up.setParam("literal.bbox", "ENVELOPE(" + bboxValues[0] + ", "
+                        + bboxValues[1] + ", " + bboxValues[2] + ", " + bboxValues[3] + ")");
+            }
+            up.setParam("fmap.content", "attr_content");
+            up.setAction(AbstractUpdateRequest.ACTION.COMMIT, true, true);
+            solr.request(up);
         } 
+
+        PrintWriter writer = new PrintWriter("temp.txt", "UTF-8");
+        for (pageRankUrl temp : graph) {
+            if (urlDidMap.containsKey(temp.getInLink()) && urlDidMap.containsKey(temp.getBaseUrl())
+                    && !urlDidMap.get(temp.getInLink()).equals("dummyDid")
+                    && !urlDidMap.get(temp.getBaseUrl()).equals("dummyDid")) {
+                writer.println(urlDidMap.get(temp.getInLink()) + " " + urlDidMap.get(temp.getBaseUrl()) + " " + temp.getWeight());
+                    }
+        }
+        writer.close();
+
+        QueryResponse rsp = solr.query(new SolrQuery("*:*"));
+        SolrDocumentList results = rsp.getResults();
+        System.out.println("--------------------------------------------------------------------------------------------------------------");
+        for (int k=0; k < results.size(); k++) {
+            System.out.println(results.get(k).getFieldNames());
+        }
     }
 }
