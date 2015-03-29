c07c8e3cd434e4b2a5f12703cfa51ef99b6ad9d4
Jayavanth Shenoy <jay@Jayavanths-MacBook-Pro.local>
2015-03-28 18:38:59 -0700

    pagerank

---
+++ pagerank_networkx/pagerank.py
@@ -0,0 +1,18 @@
+import networkx as nx
+
+
+D = nx.DiGraph()
+
+ltpl = list()
+with open('input.txt','r') as fp:
+	for line in fp:
+		frm,to,w = line.split(' ',3)
+		tpl = (frm,to,float(w))
+		ltpl.append(tpl)
+
+	D.add_weighted_edges_from(ltpl)
+
+pr = nx.pagerank(D)
+
+for x in pr:
+	print x + ' ' + str(pr[x])
